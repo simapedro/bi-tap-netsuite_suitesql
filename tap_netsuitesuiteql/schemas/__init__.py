2f2619b585d67cb2f28c0421b045c4ea82f94f7a
Simão Vale Moreira <smoreira@broadvoice.com>
2025-06-03 14:40:13 +0100

    update schema

--- tap_netsuitesuiteql/schemas/__init__.py
+++ tap_netsuitesuiteql/schemas/__init__.py
@@ -1,1 +1,9 @@
 """JSON schema files for the REST API."""
+{
+  "currencyRate": [
+    {"displayName": "basecurrency", "name": "basecurrency", "type": "string"},
+    {"displayName": "effectivedate", "name": "effectivedate", "type": "string"},
+    {"displayName": "exchangerate", "name": "exchangerate", "type": "string"},
+    {"displayName": "transactionCurrency", "name": "transactionCurrency", "type": "string"}
+  ]
+}
