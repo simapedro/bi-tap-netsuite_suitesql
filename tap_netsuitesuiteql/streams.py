e85f8ab76f2b4cade4b91416346624cd4934ae27
Simão Vale Moreira <smoreira@broadvoice.com>
2025-06-04 10:41:17 +0100

    update stream

--- tap_netsuitesuiteql/streams.py
+++ tap_netsuitesuiteql/streams.py
@@ -21,13 +21,13 @@ class CurrencyRateStream(NetsuiteSuiteQLStream):
 
     name = "currencyRate"
     path = ""
-    query = "SELECT BUILTIN.DF(basecurrency) AS baseCurrency, effectivedate, exchangerate, BUILTIN.DF(transactionCurrency) AS transactionCurrency FROM currencyRate WHERE effectiveDate = '" + datetime.today().strftime('%m-%d-%Y') + "'"
+    query = "SELECT BUILTIN.DF(basecurrency) AS baseCurrency, effectivedate, exchangerate, BUILTIN.DF(transactioncurrency) AS transactioncurrency FROM currencyRate WHERE effectiveDate = '" + datetime.today().strftime('%m-%d-%Y') + "'"
     replication_key = None
 
     schema = th.PropertiesList(
         th.Property("basecurrency", th.StringType),
         th.Property("effectivedate", th.DateType),
         th.Property("exchangerate", th.StringType),
-        th.Property("transactionCurrency", th.StringType),
+        th.Property("transactioncurrency", th.StringType),
 
     ).to_dict()
